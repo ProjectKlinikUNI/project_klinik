5695016bee756c6656200e742e32f9ed8612bb49
islahatunn <islahatunnufusi07@gmail.com>
2021-11-20 18:04:13 +0700

    mengubah_id_pasein

--- application/models/input_tindakan_model.php
+++ application/models/input_tindakan_model.php
@@ -8,13 +8,13 @@ class input_tindakan_model extends CI_Model
         $session = $this->db->get_where('sys_users', ['username' => $this->session->userdata('username')])
             ->row_array();
         $s = $session['username'];
-        $v = "SELECT *, dm_dokter.nama_dokter, dm_poli.nama_poli, dm_pasien.id as id_pasien FROM tbl_kunjungan JOIN dm_dokter ON dm_dokter.id_dokter = tbl_kunjungan.id_dokter JOIN dm_poli ON dm_poli.id_poli = tbl_kunjungan.id_poli JOIN dm_pasien ON dm_pasien.id = tbl_kunjungan.id_pasien WHERE tbl_kunjungan.id_dokter='$session' ";
+        $v = "SELECT *, dm_dokter.nama_dokter, dm_poli.nama_poli, dm_pasien.id_pasien as id_pasien FROM tbl_kunjungan JOIN dm_dokter ON dm_dokter.id_dokter = tbl_kunjungan.id_dokter JOIN dm_poli ON dm_poli.id_poli = tbl_kunjungan.id_poli JOIN dm_pasien ON dm_pasien.id_pasien = tbl_kunjungan.id_pasien WHERE tbl_kunjungan.id_dokter='$session' ";
         return $this->db->query($v)->result_array();
     }
     public function viewById()
     {
         $id = $this->input->post('id');
-        $v = "SELECT *, dm_dokter.nama_dokter, dm_poli.nama_poli, dm_pasien.id as id_pasien FROM tbl_kunjungan JOIN dm_dokter ON dm_dokter.id_dokter = tbl_kunjungan.id_dokter JOIN dm_poli ON dm_poli.id_poli = tbl_kunjungan.id_poli JOIN dm_pasien ON dm_pasien.id = tbl_kunjungan.id_pasien WHERE tbl_kunjungan.id_kunjungan='$id' ";
+        $v = "SELECT *, dm_dokter.nama_dokter, dm_poli.nama_poli, dm_pasien.id_pasien as id_pasien FROM tbl_kunjungan JOIN dm_dokter ON dm_dokter.id_dokter = tbl_kunjungan.id_dokter JOIN dm_poli ON dm_poli.id_poli = tbl_kunjungan.id_poli JOIN dm_pasien ON dm_pasien.id_pasien = tbl_kunjungan.id_pasien WHERE tbl_kunjungan.id_kunjungan='$id' ";
         return $this->db->query($v)->row_array();
     }
     public function update()
