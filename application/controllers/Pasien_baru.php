b37330c2fdb30bf6bb6a16767bfaa2f93627bd7f
ummi2401 <ummiriumikazul@gmail.com>
2021-11-22 19:28:44 +0700

    Update Pasien_baru.php

--- application/controllers/Pasien_baru.php
+++ application/controllers/Pasien_baru.php
@@ -3,29 +3,29 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Pasien_baru extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('pasien_baru_model', 'pasien');
-    }
+    // public function __construct()
+    // {
+    //     parent::__construct();
+    //     $this->load->model('pasien_baru_model', 'pasien');
+    // }
 
-    public function index()
-    {
-        $data['pasien'] = $this->pasien->view();
-        $data['list'] = $this->pasien->listPasien();
-        $data['title'] = 'Pasien';
-        $data['content_wrapper'] = $this->load->view('pasien/pasien_baru', $data, true);
-        $this->load->view('main', $data);
-    }
-    public function create()
-    {
-        $this->pasien->create();
-        $this->session->flashdata('Message', 'Data Pasien Berhasil Ditambahkan');
-        redirect('pasien_baru');
-    }
-    public function printPasien()
-    {
-        $this->pasien->create();
-        redirect('Pasien');
-    }
+    // public function index()
+    // {
+    //     $data['pasien'] = $this->pasien->view();
+    //     $data['list'] = $this->pasien->listPasien();
+    //     $data['title'] = 'Pasien';
+    //     $data['content_wrapper'] = $this->load->view('pasien/pasien_baru', $data, true);
+    //     $this->load->view('main', $data);
+    // }
+    // public function create()
+    // {
+    //     $this->pasien->create();
+    //     $this->session->flashdata('Message', 'Data Pasien Berhasil Ditambahkan');
+    //     redirect('pasien_baru');
+    // }
+    // public function printPasien()
+    // {
+    //     $this->pasien->create();
+    //     redirect('Pasien');
+    // }
 }
