d31f40764180305e20bedf428bbb8c652a443ff9
islahatunn <islahatunnufusi07@gmail.com>
2021-11-28 00:05:10 +0700

    perbaikan

--- application/views/tindakan/input_tindakan.php
+++ application/views/tindakan/input_tindakan.php
@@ -1,50 +1,50 @@
 <div class="content-wrapper mt-5">
-<<<<<<< HEAD
+
     <div class="container-fluid ">
-        <button class="btn btn-info ml-3 mt-3" data-toggle="modal" data-target="#create">Tambah Pasien Baru</button>
-=======
-    <div class="container ">
->>>>>>> bc3f19228d831a8aaaaebdf90b55ad6e6a901926
-        <div class="card border-info ml-3 mt-3">
-            <!-- <div class="card-header "></div> -->
-            <div class="card-body ">
-                <table id="table" class="table table-striped table-bordered" style="width:100%">
-                    <thead>
-                        <tr class="align-middle">
-                            <th>NO</th>
-                            <th>NO ANTRIAN</th>
-                            <th>TANGGAL PENDAFTARAN</th>
-                            <th>ID PASIEN</th>
-                            <th>NAMA PASIEN</th>
-                            <th>JENIS POLI</th>
-                            <th>JENIS LAYANAN</th>
-                            <th>DOKTER</th>
-                            <th>AKSI</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $i = 1;
-                        foreach ($view as $v) :
-                        ?>
-                            <tr>
-                                <td><?= $i; ?></td>
-                                <td><?= $v['id_kunjungan'] ?></td>
-                                <td><?= $v['tanggal_kunjungan'] ?></td>
-                                <td><?= $v['id_pasien'] ?></td>
-                                <td><?= $v['nama_pasien'] ?></td>
-                                <td><?= $v['nama_poli'] ?></td>
-                                <td><?= $v['layanan'] ?></td>
-                                <td><?= $v['nama_dokter'] ?></td>
-                                <td class="text-center"><a href="<?= base_url('Input_tindakan/update/') . $v['id_kunjungan'] ?>"><i class="far fa-edit"></i></a></td>
+        <button class="btn btn-info ml-3 mt-3" data-toggle="modal" data-target="#create">Daftar Pasien Baru</button>
+
+        <div class="container ">
+
+            <div class="card border-info ml-3 mt-3">
+                <!-- <div class="card-header "></div> -->
+                <div class="card-body ">
+                    <table id="table" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr class="align-middle">
+                                <th>NO</th>
+                                <th>NO ANTRIAN</th>
+                                <th>TANGGAL PENDAFTARAN</th>
+                                <th>ID PASIEN</th>
+                                <th>NAMA PASIEN</th>
+                                <th>JENIS POLI</th>
+                                <th>JENIS LAYANAN</th>
+                                <th>DOKTER</th>
+                                <th>AKSI</th>
                             </tr>
-                    </tbody>
+                        </thead>
+                        <tbody>
+                            <?php $i = 1;
+                            foreach ($view as $v) :
+                            ?>
+                                <tr>
+                                    <td><?= $i; ?></td>
+                                    <td><?= $v['id_kunjungan'] ?></td>
+                                    <td><?= $v['tanggal_kunjungan'] ?></td>
+                                    <td><?= $v['id_pasien'] ?></td>
+                                    <td><?= $v['nama_pasien'] ?></td>
+                                    <td><?= $v['nama_poli'] ?></td>
+                                    <td><?= $v['layanan'] ?></td>
+                                    <td><?= $v['nama_dokter'] ?></td>
+                                    <td class="text-center"><a href="<?= base_url('Input_tindakan/update/') . $v['id_kunjungan'] ?>"><i class="far fa-edit"></i></a></td>
+                                </tr>
+                        </tbody>
 
-                <?php $i++;
-                        endforeach;
-                ?>
+                    <?php $i++;
+                            endforeach;
+                    ?>
 
-                </table>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
